8690fb58a7f4683ded57ec6caf38693a39e51344
mengsifei <victoriameng2013@gmail.com>
2023-05-22 16:27:06 +0300

    ratio changed

--- src/secondary_check.py
+++ src/secondary_check.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def detect_blur(image):
-    score = np.var(cv2.Laplacian(image, cv2.CV_16S))
+    score = np.var(cv2.Laplacian(image, cv2.CV_8U))
     return score
 
 
@@ -30,38 +30,30 @@ def looking_center(gray, shape, side, debug=False):
         return -2
     # cv2_imshow(threshold_eye)
     height, width = threshold_eye.shape
-    left_threshold = threshold_eye[0: height, 0: int(width / 2)]
-    right_threshold = threshold_eye[0: height, int(width / 2): width]
-    left_white = cv2.countNonZero(threshold_eye[0: height, 0: int(width / 4)])
-    left_black = height * int(width / 2) - cv2.countNonZero(left_threshold)
-    right_white = cv2.countNonZero(threshold_eye[0: height, int(3 * width / 4): width])
-    right_black = height * int(width / 2) - cv2.countNonZero(right_threshold)
-    down_threshold = threshold_eye[int(4 * height / 5): height, 0: width]
-    down_black = down_threshold.shape[0] * down_threshold.shape[1] - cv2.countNonZero(down_threshold)
+    left_side_threshold = threshold_eye[0: height, 0: int(width / 2)]
+    left_side_black = height * int(width / 2) - cv2.countNonZero(left_side_threshold)
+    right_side_threshold = threshold_eye[0: height, int(width / 2): width]
+    right_side_black = height * int(width / 2) - cv2.countNonZero(right_side_threshold)
+    up_side_threshold = threshold_eye[0: int(height / 2), 0: width]
+    up_side_black = int(height / 2) * width - cv2.countNonZero(up_side_threshold)
+    down_side_threshold = threshold_eye[int(height / 2): height, 0: width]
+    down_side_black = height * int(width / 2) - cv2.countNonZero(down_side_threshold)
     white = cv2.countNonZero(threshold_eye)
     black = height * width - white
+    if (white == 0) or (down_side_black < 10) or (up_side_black < 10) or (black / white) < 0.4:
+        if debug:
+            print("Looking up or no pupil")
+        return -2
+    if right_side_black == 0 or left_side_black == 0:
+        if debug:
+            print("Looking extremely left or right")
+        return -2
+    gaze_ratio_horizontal = left_side_black / right_side_black
     if debug:
-        cv2_imshow(threshold_eye)
-        print(side, "black", black, "white", white, "ratio", black / white, "left white", left_white, "right_white",
-              right_white, "down_black", down_black)
-    if (white == 0) or (down_black <= 10):
-        if debug:
-            print("Looking up")
-        return -2
-
-    if (black / white) < 0.4:
-        if debug:
-            print("no pupil")
-        return -2
-    if right_white < 3 or left_white < 3 or right_black == 0:
-        if debug:
-            print("looking extreme left or right")
-        return -2
-    gaze_ratio_horizontal = left_black / right_black
-    # print(side, "gaze_ratio_horizontal", gaze_ratio_horizontal)
+        print("Ratio", gaze_ratio_horizontal, "Black", black, "White", white)
     if int(left_best <= gaze_ratio_horizontal <= right_best):
         return 1
     elif int(left_good <= gaze_ratio_horizontal <= right_good):
-        return 0.5
+        return 0.8
     else:
         return -1
